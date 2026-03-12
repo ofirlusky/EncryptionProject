7b20e26c88d98f3f8f473f54ecde723c78e6fabb
USER <ofirlu5g@gmail.com>
2026-03-12 19:26:20 +0200

    picaria is ready

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dgthhterkjlwjwjwjwjwjwjwjwjbjjjjjjjjjjjjjjjbjbjbjj");
+        SwingUtilities.invokeLater(() -> {
+            // יוצרים את המשחק
+            PicariaGame game = new PicariaGame();
 
+            // מריצים את ה-GUI
+            PicariaGUI gui = new PicariaGUI(game);
+            gui.setVisible(true);
+        });
     }
 }
