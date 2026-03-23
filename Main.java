3db4d729198b4fd7f074034d19a223d8917d4d03
USER <ofirlu5g@gmail.com>
2026-03-23 14:59:58 +0200

    start from scrach

--- Main.java
+++ Main.java
@@ -2,13 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            // יוצרים את המשחק
-            PicariaGame game = new PicariaGame();
 
-            // מריצים את ה-GUI
-            PicariaGUI gui = new PicariaGUI(game);
-            gui.setVisible(true);
-        });
     }
 }
