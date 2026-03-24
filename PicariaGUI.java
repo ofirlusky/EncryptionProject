d86effca1844c1dd141029a871b20cdbd2f2a9e3
USER <ofirlu5g@gmail.com>
2026-03-24 17:23:46 +0200

    add grapics

---
+++ PicariaGUI.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PicariaGUI extends JFrame {
+
+    private PicariaGame game;
+    private JLabel statusLabel;
+
+    public PicariaGUI() {
+        setTitle("The Game Of The Lusexy");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(620, 700);
+        setLayout(new BorderLayout());
+
+
+        ImageIcon image = new ImageIcon("image.png");
+        setIconImage(image.getImage());
+
+
+        game = new PicariaGame();
+
+        statusLabel = new JLabel("Player 1 (Blue) - Drop Phase", SwingConstants.CENTER);
+        statusLabel.setFont(new Font("Arial", Font.BOLD, 22));
+        statusLabel.setOpaque(true);
+        statusLabel.setBackground(Color.BLACK);
+        statusLabel.setForeground(Color.WHITE);
+        statusLabel.setBorder(BorderFactory.createEmptyBorder(15, 0, 15, 0));
+
+        BoardPanel boardPanel = new BoardPanel(game, statusLabel);
+        add(statusLabel, BorderLayout.NORTH);
+        add(boardPanel, BorderLayout.CENTER);
+
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+
+
+}
+
+
+
+
--- PicariaGame.java
+++ PicariaGame.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -6,142 +5,105 @@ import java.util.Map;
 
 public class PicariaGame {
 
-    public static final int TOTAL_NODES=13;
-    private Node board[]; // 0 empty , 1 player1 , 2 player2
-    private int directions[];
+    public static final int TOTAL_NODES = 13;
+    public static final int[][] neighbors = {
+            {1, 3, 5}, {0, 2, 3, 4, 6}, {1, 4, 7}, {0, 1, 5, 6}, {1, 2, 6, 7},
+            {0, 3, 6, 8, 10}, {1, 3, 4, 5, 7, 8, 9, 11}, {2, 4, 6, 9, 12},
+            {5, 6, 10, 11}, {6, 7, 11, 12}, {5, 8, 11}, {6, 8, 9, 10, 12}, {7, 9, 11}
+    };
+    public static final int[][] COORDS = {
+            {0,0}, {2,0}, {4,0}, {1,1}, {3,1}, {0,2}, {2,2}, {4,2}, {1,3}, {3,3}, {0,4}, {2,4}, {4,4}
+    };
+
+
+    private Node[] board;
     private Player playerNumberOne;
     private Player playerNumberTwo;
     private PlayerID currentTurn;
-
     private int numberOfpiecesPlaced = 0;
     private Node selectedNode = null;
     private boolean isGameOver = false;
-
-
-    private final int[][] neighbors = {
-            {1, 3, 5},                // 0
-            {0, 2, 3, 4, 6},          // 1
-            {1, 4, 7},                // 2
-            {0, 1, 5, 6},             // 3
-            {1, 2, 6, 7},             // 4
-            {0, 3, 6, 8, 10},         // 5
-            {1, 3, 4, 5, 7, 8, 9, 11},// 6
-            {2, 4, 6, 9, 12},         // 7
-            {5, 6, 10, 11},           // 8
-            {6, 7, 11, 12},           // 9
-            {5, 8, 11},               // 10
-            {6, 8, 9, 10, 12},        // 11
-            {7, 9, 11}                // 12
-    };
-
-    private final int[][] COORDS = {
-            {0,0}, {2,0}, {4,0},
-            {1,1}, {3,1},
-            {0,2}, {2,2}, {4,2},
-            {1,3}, {3,3},
-            {0,4}, {2,4}, {4,4}
-    };
-
     private Map<Direction, List<Integer>>[] neighborsByDir;
-    // זה מערך שכל תא הוא מילון ובתוכו יש מיפוי של כיוונים והשכנים , יצרתי אותו בשביל אלגוריתם לבדיקת נצחון טוב ולא מעפן
-
-
-
-
 
     public PicariaGame() {
         this.board = new Node[TOTAL_NODES];
-        for(int i=0; i< PicariaGame.TOTAL_NODES; i++)
-        {
+        for(int i = 0; i < TOTAL_NODES; i++) {
             this.board[i] = new Node(i);
         }
         this.playerNumberOne = new Player(PlayerID.PLAYER_ONE);
         this.playerNumberTwo = new Player(PlayerID.PLAYER_TWO);
         this.currentTurn = PlayerID.PLAYER_ONE;
+        this.isGameOver = false;
+        this.numberOfpiecesPlaced = 0;
 
         buildNeighborsByDir();
     }
 
-
     private void buildNeighborsByDir() {
         neighborsByDir = new HashMap[TOTAL_NODES];
-
         for (int i = 0; i < TOTAL_NODES; i++) {
             neighborsByDir[i] = new HashMap<>();
-            for (int j=0; j < neighbors[i].length; j++) {
-                int neighborId = neighbors[i][j];
-
+            for (int neighborId : neighbors[i]) {
                 int dx = COORDS[neighborId][0] - COORDS[i][0];
                 int dy = COORDS[neighborId][1] - COORDS[i][1];
-
                 Direction dir = getDirection(dx, dy);
-
                 neighborsByDir[i].computeIfAbsent(dir, k -> new ArrayList<>()).add(neighborId);
             }
         }
     }
 
-
     private Direction getDirection(int dx, int dy) {
-        if (dy == 0) return Direction.HORIZONTAL;
-        if (dx == 0) return Direction.VERTICAL;
-        if ((dx > 0) == (dy > 0)) return Direction.DIAG_DOWN_RIGHT;
+        if (dy == 0)
+        {
+            return Direction.HORIZONTAL;
+        }
+        if (dx == 0)
+        {
+            return Direction.VERTICAL;
+        }
+        if ((dx > 0) == (dy > 0))
+        {
+            return Direction.DIAG_DOWN_RIGHT;
+        }
         return Direction.DIAG_DOWN_LEFT;
     }
 
-    public Map<Direction, List<Integer>>[] getNeighborsByDir() {
-        return neighborsByDir;
-    }
 
-
-    public void platTurn(int nodeID)
-    {
-        if(isGameOver)
-        {
-            System.out.println("game is fucking over");
+    public void playTurn(int nodeID) {
+        if(isGameOver) {
+            System.out.println("The game is already over!");
             return;
         }
+
         Node clickedNode = board[nodeID];
-        if(numberOfpiecesPlaced < 6)
-        {
+
+        if(numberOfpiecesPlaced < 6) {
             handleDropPhase(clickedNode);
-        }
-        else
-        {
+        } else {
             handleMovePhase(clickedNode);
         }
     }
 
-    private void handleDropPhase(Node clickedNode)
-    {
-        if(GameRules.isPeaceFree(clickedNode))
-        {
-            if(currentTurn == PlayerID.PLAYER_ONE)
-            {
+    private void handleDropPhase(Node clickedNode) {
+        if(GameRules.isPeaceFree(clickedNode)) {
+            if(currentTurn == PlayerID.PLAYER_ONE) {
                 clickedNode.setCurrentPieceValue(PieceValue.OCCUPIED_P1);
                 playerNumberOne.updateNumberOfSoldiers();
-
-
-            }
-            else {
+            } else {
                 clickedNode.setCurrentPieceValue(PieceValue.OCCUPIED_P2);
                 playerNumberTwo.updateNumberOfSoldiers();
-
             }
+
             numberOfpiecesPlaced++;
             checkEndGame();
-            if(!isGameOver)
-            {
+            if(!isGameOver) {
                 switchTurn();
             }
+        } else {
+            System.out.println("Slot already occupied!");
         }
-        else {
-            System.out.println("can not drope there");
-        }
-
     }
 
-    // פה הוא מטפל לי בשני לחיצות באותו פונקציה
     private void handleMovePhase(Node clickedNode) {
         if (selectedNode == null) {
             if (isCurrentPlayerPiece(clickedNode)) {
@@ -155,7 +117,6 @@ public class PicariaGame {
             }
 
             if (GameRules.isValidMove(selectedNode, clickedNode, neighbors)) {
-
                 clickedNode.setCurrentPieceValue(selectedNode.getCurrentPieceValue());
                 selectedNode.setCurrentPieceValue(PieceValue.EMPTY);
                 selectedNode = null;
@@ -170,107 +131,32 @@ public class PicariaGame {
         }
     }
 
-
     private void switchTurn() {
-        if (currentTurn == PlayerID.PLAYER_ONE) {
-            currentTurn = PlayerID.PLAYER_TWO;
-        } else {
-            currentTurn = PlayerID.PLAYER_ONE;
-        }
+        currentTurn = (currentTurn == PlayerID.PLAYER_ONE) ? PlayerID.PLAYER_TWO : PlayerID.PLAYER_ONE;
     }
 
     private void checkEndGame() {
         PlayerID winner = GameRules.checkWin(board, neighborsByDir);
         if (winner != null) {
             isGameOver = true;
-            System.out.println("!!! WE HAVE A WINNER: " + winner + " !!!");
         }
     }
 
     private boolean isCurrentPlayerPiece(Node node) {
-        if (currentTurn == PlayerID.PLAYER_ONE && node.getCurrentPieceValue() == PieceValue.OCCUPIED_P1) {
-            return true;
-        }
-        if (currentTurn == PlayerID.PLAYER_TWO && node.getCurrentPieceValue() == PieceValue.OCCUPIED_P2) {
-            return true;
-        }
+        if (currentTurn == PlayerID.PLAYER_ONE && node.getCurrentPieceValue() == PieceValue.OCCUPIED_P1) return true;
+        if (currentTurn == PlayerID.PLAYER_TWO && node.getCurrentPieceValue() == PieceValue.OCCUPIED_P2) return true;
         return false;
     }
 
-
-    public Node[] getBoard() {
-        return board;
-    }
-
-    public void setBoard(Node[] board) {
-        this.board = board;
-    }
-
-    public int[] getDirections() {
-        return directions;
-    }
-
-    public void setDirections(int[] directions) {
-        this.directions = directions;
-    }
-
-    public Player getPlayerNumberOne() {
-        return playerNumberOne;
-    }
-
-    public void setPlayerNumberOne(Player playerNumberOne) {
-        this.playerNumberOne = playerNumberOne;
-    }
-
-    public Player getPlayerNumberTwo() {
-        return playerNumberTwo;
-    }
-
-    public void setPlayerNumberTwo(Player playerNumberTwo) {
-        this.playerNumberTwo = playerNumberTwo;
-    }
-
-    public PlayerID getCurrentTurn() {
-        return currentTurn;
-    }
-
-    public void setCurrentTurn(PlayerID currentTurn) {
-        this.currentTurn = currentTurn;
-    }
+    // Getters
+    public Node[] getBoard() { return board; }
+    public PlayerID getCurrentTurn() { return currentTurn; }
+    public boolean isGameOver() { return isGameOver; }
+    public Node getSelectedNode() { return selectedNode; }
 
     public int getNumberOfpiecesPlaced() {
         return numberOfpiecesPlaced;
     }
 
-    public void setNumberOfpiecesPlaced(int numberOfpiecesPlaced) {
-        this.numberOfpiecesPlaced = numberOfpiecesPlaced;
-    }
-
-    public Node getSelectedNode() {
-        return selectedNode;
-    }
-
-    public void setSelectedNode(Node selectedNode) {
-        this.selectedNode = selectedNode;
-    }
-
-    public boolean isGameOver() {
-        return isGameOver;
-    }
-
-    public void setGameOver(boolean gameOver) {
-        isGameOver = gameOver;
-    }
-
-    public int[][] getNeighbors() {
-        return neighbors;
-    }
-
-    public int[][] getCOORDS() {
-        return COORDS;
-    }
-
-    public void setNeighborsByDir(Map<Direction, List<Integer>>[] neighborsByDir) {
-        this.neighborsByDir = neighborsByDir;
-    }
+    public Map<Direction, List<Integer>>[] getNeighborsByDir() { return neighborsByDir; }
 }
